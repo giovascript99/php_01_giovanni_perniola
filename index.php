ea91dd71ace273ccad321b606a3d61b27b5a5777
Giovanni Perniola <gioperniola1999@gmail.com>
2025-10-30 04:58:15 +0100

    variabili trasformate in costanti

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-$integer = 10;
-$float = 10.5;
-$string = "Stringa";
-$boolean = true;
+const INTERO = 10;
+const VIRGOLA = 10.5;
+const STRINGA = "STRINGa";
+const BOOLEANO = true;
 
-var_dump($integer);
-var_dump($float);
-var_dump($string);
-var_dump($boolean);
+var_dump(INTERO);
+var_dump(VIRGOLA);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
